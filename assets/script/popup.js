b1773a6a06af2d606b3d5ca80fd044b98fb3387d
ausrazali <109255635+ausrazali@users.noreply.github.com>
2023-12-07 23:18:17 +0800

    Added script to execute when user click Need Access and when user click Close button in the popup message

--- assets/script/popup.js
+++ assets/script/popup.js
@@ -1,13 +1,21 @@
-let needAccessMessage = document.getElementById("need-access");
-let popupMessage = document.getElementById("access-popup")
-let popupCloseButton = document.getElementById("popup-close");
+let needAccess320pxClicker = document.getElementById("need-access-320px-clicker");
+let needAccessMessage320px = document.getElementById("need-access-message-320px");
+let topGrid = document.getElementById("top-grid");
+let middleGrid = document.getElementById("middle-grid");
+let bottomGrid = document.getElementById("bottom-grid");
 
-needAccessMessage.addEventListener("click", function() {
-    setTimeout(() => {
-        popupMessage.style.display = "grid";
-    }, 300);
+needAccess320pxClicker.addEventListener("click", function() {
+    topGrid.style.display = "none";
+    middleGrid.style.display = "none";
+    bottomGrid.style.display = "none";
+    needAccessMessage320px.style.display = "grid";
 });
 
-popupCloseButton.addEventListener("click", function() {
-    popupMessage.style.display = "none";
+let needAccessMessage320pxButtonClose = document.getElementById("need-access-message-320px-button-close")
+
+needAccessMessage320pxButtonClose.addEventListener("click", function() {
+    topGrid.style.display = "grid";
+    middleGrid.style.display = "grid";
+    bottomGrid.style.display = "grid";
+    needAccessMessage320px.style.display = "none";
 });
